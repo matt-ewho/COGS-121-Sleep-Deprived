650c6898cbaee0891c323dd78c6a21c336ae37a7
yhilke <32581135+yhilke@users.noreply.github.com>
2018-05-14 16:00:45 -0700

    whitelist database

--- database.js
+++ database.js
@@ -10,6 +10,7 @@
 
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('sites.db');
+const wdb = new sqlite3.Database('whitelist.db');
 
 db.serialize(() => {
 
@@ -21,8 +22,32 @@ db.serialize(() => {
   console.log('created database of example sites');
 
   db.each("SELECT hours, firstSite, sndSite FROM visited_sites", (err, row) => {
-      console.log(row.hours + ": " + row.firstSite + ' - ' + row.sndSite);
+    console.log(row.hours + ": " + row.firstSite + ' - ' + row.sndSite);
   });
+
+  db.close();
+
 });
 
-db.close();
+wdb.serialize(() => {
+  //creating white list database
+
+  wdb.run("CREATE TABLE whitelist_sites (whiteSite TEXT)");
+  //create whitelist table
+
+
+  wdb.run("INSERT INTO whitelist_sites VALUES ('ted.ucsd.edu')");
+  wdb.run("INSERT INTO whitelist_sites VALUES ('scholar.google.com')");
+  wdb.run("INSERT INTO whitelist_sites VALUES ('wikipedia.org')");
+  //inserted all pre-whitelisted values
+
+  console.log('created database of pre-whitelisted sites');
+
+   db.each("SELECT whiteSite FROM whitelist_sites", (err, row) => {
+      //console.log(row.whiteSite);
+      //for some reason the console log isn't working
+  });
+
+  wdb.close();
+
+});
