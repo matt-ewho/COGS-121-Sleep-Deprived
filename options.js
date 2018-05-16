ad221361f10b727225ef153afd865a06e0eced6d
yhilke <32581135+yhilke@users.noreply.github.com>
2018-05-15 19:12:45 -0700

    added firebase
    added the firebase code and added code to manipulate the database when buttons are clicked. firebase doesn't work for some reason =(

--- options.js
+++ options.js
@@ -1,3 +1,23 @@
+  //initialize firebase
+     var config = {
+      apiKey: "AIzaSyCo8rjvxcOKTgHNXhQqcmSsi6EmG2h1SgA",
+      authDomain: "cogs121-830e5.firebaseapp.com",
+      databaseURL: "https://cogs121-830e5.firebaseio.com",
+      projectId: "cogs121-830e5",
+      storageBucket: "cogs121-830e5.appspot.com",
+      messagingSenderId: "1030210360810"
+    };
+ firebase.initializeApp(config);
+
+ const whitelist = firebase.database();
+ const timeInt = firebase.database();
+
+/*
+//require is not working even after adding require.js
+const sqlite3 = require('sqlite3');
+const wdb = new sqlite3.Database('whitelist.db');
+*/
+
 document.addEventListener("DOMContentLoaded", function(event) {
   const optionsURL = window.location.href;
   console.log("ready!");
@@ -17,9 +37,19 @@ document.getElementById("alertbutton").onclick=function() {
 
 document.getElementById("deleteWhitelist").onclick=function() {
   if (confirm("Are you sure you wish to delete all of your whitelisted websites?")) {
+    //deletes entire whitelist
+    whitelist.remove();
     alert("Whitelisted websites have been deleted.");
   }
 };
+
+//alert will tell what site was added
+document.getElementById("addWhitelist").onclick=function() {
+  const addedSite = document.getElementById("whitelist-add-url").value;
+  console.log(addedSite);
+  //add to database using push function
+  alert(addedSite+ " saved into database!");
+}
 
 /*SAVE CHANGES alerts*/
 document.getElementById("saveWhitelist").onclick=function() {
@@ -27,7 +57,10 @@ document.getElementById("saveWhitelist").onclick=function() {
 };
 
 document.getElementById("saveInterval").onclick=function () {
-  alert("Work Interval settings have been saved successfully.");
+  const saveInterval = document.getElementById("intervalSelect").value;
+  console.log(saveInterval);
+  timeInt.ref('timeInt/').set(saveInterval);
+  alert("Work Interval settings have been set to: " + saveInterval);
 };
 
 document.getElementById("saveAll").onclick=function () {
