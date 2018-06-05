09ab4e54c291a1e37aabd4aacdc6b11e85fe7c0e
Matthew Ho <h5ho@ucsd.edu>
2018-06-05 02:11:32 -0700

    chart shows but what about the values

--- popup.js
+++ popup.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 });
 
-/* debugging function for resetting database 
+/* debugging function for resetting database
 document.getElementById("resetDatabase").onclick=function() {
   chrome.storage.local.get(["whitelist"], function(result) {
     console.log(result);
@@ -92,18 +92,6 @@ document.getElementById("add-icon").onclick=function() {
     });
   });
 }
-/**********************************************************************************
-                                    C3 Timechart
-***********************************************************************************/
-var chart = c3.generate({
-bindto: document.getElementById("chart2"),
-data: {
-columns: [
-  ['data1', 30, 200, 100, 400, 150, 250],
-  ['data2', 50, 20, 10, 40, 15, 25]
-        ]
-      }
-});
 
 /**********************************************************************************
                                   PROGRESS BAR FUNCTIONS
